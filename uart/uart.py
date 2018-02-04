b6289c249f32fc03f99be127369b08ca962d50d3
Kenta <k.i.drivingroad21@gmail.com>
2018-02-04 12:19:04 +0900

    Modified UART Concole

--- uart/uart.py
+++ uart/uart.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 # Author: Kenta Ishii
 # SPDX short identifier: BSD-3-Clause
-# python3 ./uart.py example.txt /dev/serial0 115200 0.01
+# ./uart.py example.txt /dev/serial0 115200 0.01
 
 import sys
 import serial
@@ -18,21 +18,17 @@ char_star = "\x2A".encode("ascii", "replace") # Asterisk
 char_lf = "\x0A".encode("ascii", "replace") # Line Feed
 char_cr = "\x0D".encode("ascii", "replace") # Carriage Return
 char_nak = "\x15".encode("ascii", "replace") # NAK
-
-int_star = int.from_bytes(char_star, "little")
-int_cr = int.from_bytes(char_cr, "little")
-list_cr = [int_star, int_cr]
+esc_up = "\x1B[A".encode("ascii", "replace") # Cursor Up on Escape Sequence
+esc_down = "\x1B[B".encode("ascii", "replace") # Cursor Down on Escape Sequence
+esc_right = "\x1B[C".encode("ascii", "replace") # Cursor Right on Escape Sequence
+esc_left = "\x1B[D".encode("ascii", "replace") # Cursor Left on Escape Sequence
 
 uart = serial.Serial(port = argv[2], baudrate = int(argv[3]), timeout = float(argv[4]))
 
-def uart_send(text):
+def uart_send(uart, text, exmirror):
     for integer in text: # Integer
         char = integer.to_bytes(1, "little")
-        char_prior = uart.read(1)
-        while len(char_prior) != 0:
-            char_prior = chr(int.from_bytes(char_prior, "little"))
-            print(char_prior, end="")
-            char_prior = uart.read(1)
+        uart_receive(uart)
         flag = True
         while flag == True:
             if char == char_lf:
@@ -41,11 +37,34 @@ def uart_send(text):
             char_res = uart.read(1) # Bytes
             if char_res == char:
                 flag = False
-                char_res = chr(int.from_bytes(char_res, "little"))
-                print(char_res, end="")
+                if exmirror == True:
+                    char_res = chr(int.from_bytes(char_res, "little"))
+                    print(char_res, end="")
             elif char_res == char_nak:
                 time.sleep(0.001)
 
-uart_send(list_cr)
-uart_send(text_all)
+def uart_receive(uart):
+        char = uart.read(1)
+        while len(char) != 0:
+            char = chr(int.from_bytes(char, "little"))
+            print(char, end="")
+            char = uart.read(1)
+
+def uart_input(uart):
+        flag = True
+        while flag == True:
+            uart_receive(uart)
+            text_input = input()
+            text_input_len = len(text_input)
+            print("\x1B[A", end="")
+            if text_input_len != 0:
+                text_input += "\r"
+                text_input = text_input.encode("ascii", "replace")
+                uart_send(uart, text_input, False)
+            else:
+                flag = False # If No Input, Loop Ends
+
+uart_send(uart, text_all, True)
+uart_receive(uart)
+uart_input(uart)
 uart.close()
