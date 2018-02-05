8ece5e49c0fdad20c62db40f63a31d08c8dfef8c
Kenta <k.i.drivingroad21@gmail.com>
2018-02-05 12:40:15 +0900

    Modified UART Console, Classfied

--- uart/uart.py
+++ uart/uart.py
@@ -8,12 +8,6 @@ import sys
 import serial
 import time
 
-argv = sys.argv
-#print (argv[0]) # File Name
-
-f = open( argv[1], "rb") # Read Only
-text_all = f.read()
-
 char_star = "\x2A".encode("ascii", "replace") # Asterisk
 char_lf = "\x0A".encode("ascii", "replace") # Line Feed
 char_cr = "\x0D".encode("ascii", "replace") # Carriage Return
@@ -23,49 +17,64 @@ esc_down = "\x1B[B".encode("ascii", "replace") # Cursor Down on Escape Sequence
 esc_right = "\x1B[C".encode("ascii", "replace") # Cursor Right on Escape Sequence
 esc_left = "\x1B[D".encode("ascii", "replace") # Cursor Left on Escape Sequence
 
+class UartConsole:
+    """Dependency:time"""
+    def __init__(self, uart):
+        self.__uart = uart
+    def send(self, text, exmirror):
+        for integer in text: # Integer
+            char = integer.to_bytes(1, "little")
+            self.receive()
+            flag = True
+            while flag == True:
+                if char == char_lf:
+                   char = char_cr
+                self.__uart.write(char)
+                char_res = self.__uart.read(1) # Bytes
+                if char_res == char: # Receive Succeed
+                    flag = False
+                    if exmirror == True:
+                        char_res = chr(int.from_bytes(char_res, "little"))
+                        print(char_res, end="")
+                elif char_res == char_nak: # Receive Negative Acknowledge
+                    time.sleep(0.001)
+                else: # Receive Fails
+                    flag = False
+
+    def receive(self):
+            char = self.__uart.read(1)
+            while len(char) != 0:
+                char = chr(int.from_bytes(char, "little"))
+                print(char, end="")
+                char = self.__uart.read(1)
+
+    def input(self):
+            while True:
+                self.receive()
+                try:
+                    text_input = input()
+                except KeyboardInterrupt:
+                    break
+                print("\x1B[A", end="")
+                text_input += "\r"
+                text_input = text_input.encode("ascii", "replace")
+                self.send(text_input, False)
+
+    def __del__(self):
+        self.__uart.close()
+
+print (sys.version)
+argv = sys.argv
+print (argv[0]) # File Name
+
+fsrc = open( argv[1], "rb") # Read Only With Raw Data (Integer)
+text_all = fsrc.read()
+#fdst = open( argv[5], "w+") # Write Only With UTF-8
+
 uart = serial.Serial(port = argv[2], baudrate = int(argv[3]), timeout = float(argv[4]))
-
-def uart_send(uart, text, exmirror):
-    for integer in text: # Integer
-        char = integer.to_bytes(1, "little")
-        uart_receive(uart)
-        flag = True
-        while flag == True:
-            if char == char_lf:
-               char = char_cr
-            uart.write(char)
-            char_res = uart.read(1) # Bytes
-            if char_res == char: # Receive Succeed
-                flag = False
-                if exmirror == True:
-                    char_res = chr(int.from_bytes(char_res, "little"))
-                    print(char_res, end="")
-            elif char_res == char_nak: # Receive Negative Acknowledge
-                time.sleep(0.001)
-            else: # Receive Fails
-                flag = False
-
-def uart_receive(uart):
-        char = uart.read(1)
-        while len(char) != 0:
-            char = chr(int.from_bytes(char, "little"))
-            print(char, end="")
-            char = uart.read(1)
-
-def uart_input(uart):
-        while True:
-            uart_receive(uart)
-            try:
-                text_input = input()
-            except KeyboardInterrupt:
-                break
-            print("\x1B[A", end="")
-            text_input += "\r"
-            text_input = text_input.encode("ascii", "replace")
-            uart_send(uart, text_input, False)
-
-uart_send(uart, text_all, True)
-uart_receive(uart)
-uart_input(uart)
-uart.close()
+uartconsole = UartConsole(uart)
+uartconsole.send(text_all, True)
+uartconsole.receive()
+uartconsole.input()
+del uartconsole
 
