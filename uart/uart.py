841d83f2fdee86b1934cf129c6ad88bda0c31710
Kenta <k.i.drivingroad21@gmail.com>
2018-02-06 12:02:26 +0900

    Modified UART Console: 1. Added Function to Write Output in File

--- uart/uart.py
+++ uart/uart.py
@@ -12,6 +12,8 @@ char_star = "\x2A".encode("ascii", "replace") # Asterisk
 char_lf = "\x0A".encode("ascii", "replace") # Line Feed
 char_cr = "\x0D".encode("ascii", "replace") # Carriage Return
 char_nak = "\x15".encode("ascii", "replace") # NAK
+char_esc = "\x1B".encode("ascii", "replace") # Escape 
+char_sepa = "|".encode("ascii", "replace") # Separator
 esc_up = "\x1B[A".encode("ascii", "replace") # Cursor Up on Escape Sequence
 esc_down = "\x1B[B".encode("ascii", "replace") # Cursor Down on Escape Sequence
 esc_right = "\x1B[C".encode("ascii", "replace") # Cursor Right on Escape Sequence
@@ -19,12 +21,14 @@ esc_left = "\x1B[D".encode("ascii", "replace") # Cursor Left on Escape Sequence
 
 class UartConsole:
     """Dependency:time"""
-    def __init__(self, uart):
+    def __init__(self, uart, file):
         self.__uart = uart
-    def send(self, text, exmirror):
+        self.file = file
+
+    def send(self, text, enmirror, enwrite):
         for integer in text: # Integer
             char = integer.to_bytes(1, "little")
-            self.receive()
+            self.receive(enmirror, enwrite)
             flag = True
             while flag == True:
                 if char == char_lf:
@@ -33,7 +37,9 @@ class UartConsole:
                 char_res = self.__uart.read(1) # Bytes
                 if char_res == char: # Receive Succeed
                     flag = False
-                    if exmirror == True:
+                    if enwrite == True: # Write to File
+                        self.write(char_res)
+                    if enmirror == True: # External Mirror
                         char_res = chr(int.from_bytes(char_res, "little"))
                         print(char_res, end="")
                 elif char_res == char_nak: # Receive Negative Acknowledge
@@ -41,16 +47,26 @@ class UartConsole:
                 else: # Receive Fails
                     flag = False
 
-    def receive(self):
+    def receive(self, enmirror, enwrite):
             char = self.__uart.read(1)
             while len(char) != 0:
-                char = chr(int.from_bytes(char, "little"))
-                print(char, end="")
+                if enwrite == True:
+                    self.write(char)
+                if enmirror == True:
+                    char = chr(int.from_bytes(char, "little"))
+                    print(char, end="")
                 char = self.__uart.read(1)
+
+    def write(self,char):
+        if self.file is not None:
+            if char == char_cr:
+                return
+            char = chr(int.from_bytes(char, "little"))
+            self.file.write(char)
 
     def input(self):
             while True:
-                self.receive()
+                self.receive(True, True)
                 try:
                     text_input = input()
                 except KeyboardInterrupt:
@@ -58,7 +74,7 @@ class UartConsole:
                 print("\x1B[A", end="")
                 text_input += "\r"
                 text_input = text_input.encode("ascii", "replace")
-                self.send(text_input, False)
+                self.send(text_input, True, False)
 
     def __del__(self):
         self.__uart.close()
@@ -69,12 +85,16 @@ print (argv[0]) # File Name
 
 fsrc = open( argv[1], "rb") # Read Only With Raw Data (Integer)
 text_all = fsrc.read()
-#fdst = open( argv[5], "w+") # Write Only With UTF-8
+if len(argv) == 6: # If Sixth Argument Exists (File to Write)
+    fdst = open( argv[5], "w+") # Write Only With UTF-8
+else:
+    fdst = None;
 
 uart = serial.Serial(port = argv[2], baudrate = int(argv[3]), timeout = float(argv[4]))
-uartconsole = UartConsole(uart)
-uartconsole.send(text_all, True)
-uartconsole.receive()
+uartconsole = UartConsole(uart, fdst)
+uartconsole.send(text_all, True, False)
+uartconsole.receive(True, True)
 uartconsole.input()
+uartconsole.receive(True, True)
 del uartconsole
 
